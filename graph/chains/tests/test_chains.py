daeea27b777c904e3b36a108bda450a513c38f72
Yoganand <yoganandjgp1@gmail.com>
2026-08-10 17:26:13 +0000

    test cases for chains

--- graph/chains/tests/test_chains.py
+++ graph/chains/tests/test_chains.py
@@ -1,2 +1,24 @@
-def test_foo() -> None:
-     assert 1==1
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from graph.chains.retrieval_grader import retrieval_grader, GradeDocuments
+from ingestion import retriever
+
+def test_retrieval_grader_answer_yes() -> None:  
+     question = "agent memory"
+     docs = retriever.invoke(question)
+     doc_txt = docs[1].page_content
+     
+     res: GradeDocuments = retrieval_grader.invoke({"documents": doc_txt, "question": question})
+     assert res.binary_score == "yes"
+     
+def test_retrieval_grader_answer_no() -> None:  
+     question = "agent memory"
+     docs = retriever.invoke(question)
+     doc_txt = docs[1].page_content
+     
+     res: GradeDocuments = retrieval_grader.invoke({"documents": doc_txt, "question": "how to make pizza"})
+     assert res.binary_score == "no"
+     
+        
